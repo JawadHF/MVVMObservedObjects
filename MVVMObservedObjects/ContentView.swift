feb5c5a0c3f2280139a6522d14287e4c3899d1b7
Jawad Hussain Farooqui <jawad@winsights.in>
2020-08-25 03:55:10 +0530

    MVVM in practice

--- MVVMObservedObjects/ContentView.swift
+++ MVVMObservedObjects/ContentView.swift
@@ -8,9 +8,58 @@
 
 import SwiftUI
 
+let apiUrl = "https://someUrl/courses.json"
+
+struct Course: Identifiable, Decodable {
+    let id = UUID()
+    let name: String
+}
+class CoursesViewModel: ObservableObject{
+    
+    @Published var messages = "Messages inside observable object "
+    
+    @Published var courses: [Course] = [Course(name: "Course 1"), Course(name: "Course 2"), Course(name: "Course 3")]
+    
+    func changeMessage() {
+        self.messages = "changed message"
+    }
+        
+    func fetchCourses() {
+        //fetch json and decode and update some array property
+        guard let url = URL(string: apiUrl) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) {
+            (data, resp, err) in
+            //check error/resp
+            
+            DispatchQueue.main.async {
+                self.courses = try! JSONDecoder().decode([Course].self, from: data!)
+            }
+        }.resume()
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var coursesVM = CoursesViewModel()
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            ScrollView {
+                Text(coursesVM.messages)
+                
+                ForEach(coursesVM.courses) {
+                    course in Text(course.name)
+                }
+            }.navigationBarTitle("Courses")
+                .navigationBarItems(trailing: Button(action:{ print("Fetching json data")
+                    self.coursesVM.changeMessage()
+                    
+                }, label: { Text("Fetch Courses")
+                
+            }))
+        }
     }
 }
 
